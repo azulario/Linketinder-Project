5890b3cb41757d869294e8ba5ed6e8bd866354fe
azulario <azularioo@gmail.com>
2025-09-29 12:07:29 -0300

    build(frontend): Tailwind v4 setup; HTML usa CSS compilado

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./js/**/*.js",
+    "./ts/**/*.ts"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
